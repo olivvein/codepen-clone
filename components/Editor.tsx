0052191d601312a48782a294b38280909d283c90
Ashik K L <ashikkl19112001@gmail.com>
2023-07-08 12:38:40 +0530

    fix: styling

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { SetStateAction, useState } from "react";
+import * as Space from "react-spaces";
 
 export default function Editor() {
   const [htmlCode, setHtmlCode] = useState("");
-  const [cssCode, setCssCode] = useState("");
+  const [cssCode, setCssCode] = useState("body{background-color:black;}");
   const [jsCode, setJsCode] = useState("");
 
   const handleHtmlChange = (event: {
@@ -26,40 +27,71 @@ export default function Editor() {
   };
 
   return (
-    <div className="flex flex-col justify-center gap-8 items-center">
-      <div className="flex flex-row justify-center gap-8 items-center">
-        <div>
+    <Space.ViewPort className="w-full h-[80vh] flex flex-col">
+      <Space.Fill trackSize={true}>
+        <Space.LeftResizable
+          size="33%"
+          touchHandleSize={20}
+          trackSize={true}
+          scrollable={true}
+        >
+          <span className="leading-relaxed h-2  bg-slate-200/10 w-full  sticky  rounded-md dark:bg-slate-900/10 text-slate-900 dark:text-white">
+            HTML
+          </span>
           <textarea
-            className="bg-white dark:bg-slate-900 text-slate-900 dark:text-white"
+            className="resize-none bg-slate-200/50 w-full h-full  dark:bg-slate-900 text-slate-900 dark:text-white"
             value={htmlCode}
             onChange={handleHtmlChange}
-            placeholder="HTML"
+            placeholder="HTML code here..."
           />
-        </div>
-        <div>
+        </Space.LeftResizable>
+
+        <Space.Fill trackSize={true} scrollable={true}>
+          <span className="leading-relaxed h-2  bg-slate-200/10 w-full  sticky  rounded-md dark:bg-slate-900/10 text-slate-900 dark:text-white">
+            CSS
+          </span>
+
           <textarea
-            className="bg-white dark:bg-slate-900 text-slate-900 dark:text-white"
+            className="resize-none bg-slate-200/50 w-full h-full  dark:bg-slate-900 text-slate-900 dark:text-white"
             value={cssCode}
             onChange={handleCssChange}
-            placeholder="CSS"
+            placeholder="CSS code here..."
           />
-        </div>
-        <div>
+        </Space.Fill>
+
+        <Space.RightResizable
+          size="33%"
+          touchHandleSize={20}
+          trackSize={true}
+          scrollable={true}
+        >
+          <span className="leading-relaxed h-2  bg-slate-200/10 w-full  sticky  rounded-md dark:bg-slate-900/10 text-slate-900 dark:text-white">
+            JavaScript
+          </span>
           <textarea
-            className="bg-white dark:bg-slate-900 text-slate-900 dark:text-white"
+            className="resize-none bg-slate-200/50 w-full  h-full dark:bg-slate-900 text-slate-900 dark:text-white"
             value={jsCode}
             onChange={handleJsChange}
-            placeholder="JavaScript"
+            placeholder="JavaScript code here..."
           />
-        </div>
-      </div>
-      <div>
-        <iframe
-          className="w-screen min-h-[50vh] fixed bottom-0 left-0 max-md:mb-10"
-          aria-label="Code preview"
-          srcDoc={`<html><style>${cssCode}</style><body>${htmlCode}<script>${jsCode}</script></body></html>`}
-        />
-      </div>
-    </div>
+        </Space.RightResizable>
+      </Space.Fill>
+
+      <Space.BottomResizable
+        size="50%"
+        touchHandleSize={20}
+        trackSize={true}
+        scrollable={true}
+      >
+        <Space.Fill>
+          <iframe
+            className="w-full h-full"
+            aria-label="Code preview"
+            sandbox="allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation allow-same-origin allow-scripts"
+            srcDoc={`<html><style>${cssCode}</style><body>${htmlCode}<script>${jsCode}</script></body></html>`}
+          />
+        </Space.Fill>
+      </Space.BottomResizable>
+    </Space.ViewPort>
   );
 }
