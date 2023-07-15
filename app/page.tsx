8630b186ebdf8105e01453b0160a018480dea635
Ashik K L <ashikkl19112001@gmail.com>
2023-07-16 01:21:47 +0530

    add: homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Landing from "@/components/Landing";
 
 export default function Home() {
   return (
-    <div className="flex flex-center justify-center flex-col min-h-[50vh] w-[100%] bg-none">
-      <div className="bg-cp-color-3 w-screen h-screen z-[-1] top-0 fixed"></div>
+    <div className="flex flex-center justify-center flex-col min-h-[50vh] w-[100%] overflow-hidden">
+      <div className="bg-cp-color-3 w-screen min-h-screen h-max z-[-10] top-0 fixed"></div>
       <HomeNav />
       <Landing/>
       <Footer />
