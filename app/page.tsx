e34e13efd5d2a461513ea622fd33c12fbf9d1483
Ashik K L <74865738+ashikkl@users.noreply.github.com>
2023-07-16 22:27:46 +0530

    Fix: screen overflow

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import LandingCards from "@/components/LandingCards";
 
 export default function Home() {
   return (
-    <div className="flex flex-center justify-center flex-col min-h-[50vh] w-[100%] overflow-hidden">
+    <div className="flex flex-center justify-center flex-col min-h-[50vh] max-w-screen w-[100%] overflow-hidden">
       <div className="bg-cp-color-3 w-screen min-h-screen h-max z-[-10] top-0 fixed"></div>
       <HomeNav />
       <Landing/>
